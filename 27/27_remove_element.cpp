6d51baf7bca04f22c703ba2953444fae50508fc4
Kumar Ankush <synthaseatp@gmail.com>
2024-09-26 21:35:19 -0400

    updated cpp for missing vector lib

--- 27/27_remove_element.cpp
+++ 27/27_remove_element.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 /**
 * Remove all instances of a value from an array.
 *
 * @param nums The array to modify
-* @param numsSize The size of the array
 * @param val The value to remove
 * @return The new size of the array after removing all instances of val
 */
 
 class Solution {
 public:
-    int removeElement(int* nums, int numsSize, int val) {
+    int removeElement(vector<int>& nums, int val) {
         int i = 0;
-        for (int j=0; j<numsSize; j++) {
+        for (int j=0; j<nums.size(); j++) {
             // If the current element is not the value to remove
             if (nums[j] != val) {
                 // Copy it to the correct position
@@ -30,9 +30,8 @@ public:
 
 int main() {
     Solution sol;
-    int nums[] = {0, 1, 2, 2, 3, 3, 0, 4, 2};
+    vector<int> nums = {0, 1, 2, 2, 3, 3, 0, 4, 2};
     int val = 2;
-    int len = sizeof(nums) / sizeof(nums[0]);
-    int size = sol.removeElement(nums, len, val);
+    int size = sol.removeElement(nums, val);
     cout << size << endl;
 }
